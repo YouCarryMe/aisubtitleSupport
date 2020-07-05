ce6b2ab8a12d85e68aa6666027073c49a643d27c
Peng Yuan <37101952+YouCarryMe@users.noreply.github.com>
2020-07-05 16:19:45 +0800

    Update TencentaiInfo.java

--- src/main/java/sdu/aisubtitle/support/tencentai/TencentaiInfo.java
+++ src/main/java/sdu/aisubtitle/support/tencentai/TencentaiInfo.java
@@ -2,7 +2,7 @@ package sdu.aisubtitle.support.tencentai;
 
 public class TencentaiInfo {
 
-    public final static String secretId = "AKIDOtAlnatOK1SwFBjQgj169Fhu1xnPmw5x";
-    public final static String secretKey = "sUr2zKPFyzkFWzHMZYIm00xeXCcsugCt";
+    public final static String secretId = "";
+    public final static String secretKey = "";
 
 }
